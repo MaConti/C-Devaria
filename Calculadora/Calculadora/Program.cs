28263ade034716640bb12adf03fc261295c2a682
MaConti <matheusconti@hotmail.com>
2021-04-26 22:48:33 -0300

    1 Aula de C#

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {   
+        static float RealizarOperacao(floar primeiroNumero, float segundoNumero, string operador)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número");
+            var primeiroNumeroString = Console.ReadLine
+            float primeiroNumero;
+
+            bool isPrimeiroNumeroValido = float.TryParse(primeiroNumeroString, out primeiroNumero);
+            if (!isPrimeiroNumeroValido)
+            {
+                Console.WriteLine("Primeiro número inválido");
+                return;
+            }
+        }
+    }
+}
---
+++ TesteVarConst/TesteVarConst/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TesteVarConst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constante = "uma constante que não muda";
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Favor Informar um número inteiro");
+                return;
+            }
+
+            int inteiro;
+            bool test = int.TryParse(args[0].Trim(), out inteiro);
+
+            if(test == false)
+            {
+                Console.WriteLine("O argumento informado não é um inteiro" + args[0]);
+                       return;
+            }
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+        }
+
+
+    }
+}
